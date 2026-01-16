923065aff6b75532076f27c5d069e441999d392a
Dominik <dominik.waldburger06@gmail.com>
2026-01-16 09:11:09 +0100

    Hello World and project initialisation guide

---
+++ helloWorld/target/debug/deps/helloWorld-c78b74dd3d947f0a.d
@@ -0,0 +1,5 @@
+/home/dominik/Dokumente/Work/Rust-Learning/helloWorld/target/debug/deps/helloWorld-c78b74dd3d947f0a.d: src/main.rs
+
+/home/dominik/Dokumente/Work/Rust-Learning/helloWorld/target/debug/deps/helloWorld-c78b74dd3d947f0a: src/main.rs
+
+src/main.rs:
---
+++ helloWorld/target/debug/helloWorld.d
@@ -0,0 +1,1 @@
+/home/dominik/Dokumente/Work/Rust-Learning/helloWorld/target/debug/helloWorld: /home/dominik/Dokumente/Work/Rust-Learning/helloWorld/src/main.rs
