d0a7e50332863dbd87214b867517682698f111ad
dominik0678 <dominik.waldburger06@gmail.com>
2026-02-20 10:10:01 +0100

    tryouts

---
+++ tryouts/password_gen/src/main.rs
@@ -0,0 +1,46 @@
+use rand::seq::IndexedRandom;
+
+fn main() {
+    let new_password;
+    let lenght = 50;
+
+    new_password = password_gen(lenght);
+    println!("{}", new_password);
+
+}
+
+fn password_gen(digits: i32) -> String {
+
+    let characters = [
+        // Grossbuchstaben
+        'A','B','C','D','E','F','G','H','I','J','K','L','M',
+        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+
+        // Kleinbuchstaben
+        'a','b','c','d','e','f','g','h','i','j','k','l','m',
+        'n','o','p','q','r','s','t','u','v','w','x','y','z',
+
+        // Zahlen
+        '0','1','2','3','4','5','6','7','8','9',
+
+        // Sonderzeichen (Standard ASCII)
+        '!','"','#','$','%','&','\'','(',')','*','+',
+        ',','-','.','/',
+        ':',';','<','=','>','?',
+        '@',
+        '_',
+        '{','|','}'
+    ];
+
+
+    let mut pw = String::from("");
+    let mut rng = rand::rng();
+    
+
+    for _ in 1..=digits {
+        pw += &characters.choose(&mut rng).unwrap().to_string();
+    }
+    // 
+    //let pw = digits;
+    pw.to_string()
+}
